024690076f1d50ec08cb8d1df13755d36b199a29
pillot <benjamin.pillot@ird.fr>
2019-12-18 18:54:54 +0100

    Update regular expressions for SINAN converter

--- apureza/geocoding.py
+++ apureza/geocoding.py
@@ -4,7 +4,7 @@
 
 More detailed description.
 """
-from gistools.geocoding import DictionaryConverter, AddressBuilder
+from gistools.geocoding import DictionaryConverter
 
 __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2019, Benjamin Pillot'
@@ -16,17 +16,12 @@ class SinanConverter(DictionaryConverter):
 
     """
 
+    _converter = dict(L12={'quadra': r"(\bQ(?:C(?![UADR])|N(?![UADR]))?U?A?D?R?A?\.*\s*)(\d+)"},
+                      L13={'conjunto': r"(\bCO?N?J?U?N?T?O?\.*\s*)(\w+)"},
+                      L14={'rua': r"(\bRU?A?\.*\s*)(\w+)",
+                           'avenida': r"(\bAVE?N?I?D?A?\.*\s*)(\w+)"})
+
     def __init__(self, dictionary_file):
         super().__init__(dictionary_file)
 
 
-class SinanBuilder(AddressBuilder):
-    """ Class for building addresses from SINAN database
-
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    def build(self, *args, **kwargs):
-        pass
