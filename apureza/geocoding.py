d2189436712a3dc22d6bfef80af3aaa88fe5616b
pillot <benjamin.pillot@ird.fr>
2019-12-20 19:04:30 +0100

    Update parser

--- apureza/geocoding.py
+++ apureza/geocoding.py
@@ -4,24 +4,60 @@
 
 More detailed description.
 """
-from gistools.geocoding import DictionaryConverter
+from collections import OrderedDict
+
+import unidecode
+from gistools.geocoding import XParser
 
 __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2019, Benjamin Pillot'
 __email__ = 'benjaminpillot@riseup.net'
 
 
-class SinanConverter(DictionaryConverter):
-    """ Class for converting addresses from SINAN database
+class SinanParser(XParser):
+    """ Class for parsing addresses from SINAN database
 
     """
 
-    _converter = dict(L12={'quadra': r"(\bQ(?:C(?![UADR])|N(?![UADR]))?U?A?D?R?A?\.*\s*)(\d+)"},
-                      L13={'conjunto': r"(\bCO?N?J?U?N?T?O?\.*\s*)(\w+)"},
-                      L14={'rua': r"(\bRU?A?\.*\s*)(\w+)",
-                           'avenida': r"(\bAVE?N?I?D?A?\.*\s*)(\w+)"})
+    _address_fields = dict(logrado='NM_LOGRADO',
+                           numero='NU_NUMERO',
+                           complement='NM_COMPLEM',
+                           reference='NM_REFEREN')
 
-    def __init__(self, dictionary_file):
-        super().__init__(dictionary_file)
+    def __init__(self):
+        super().__init__()
+        self._re_parser.update(casa=r"(\b(?:k(?=sa)|c(?=a|s))a?s?a?\.*\s*[\W_]*)(\d{1,3})?",
+                               lote=r"(\blo?t?e?\.*\s*[\W_]*)(\d{1,3})",
+                               apartamento=r"(\bapa?r?t?a?m?e?n?t?o?\.*\s*)(\d{1,3})")
+
+    def parse(self, database):
+        """ Parse addresses from SINAN database
+
+        :param database: SINAN database
+        :return:
+        """
+
+        for rough_address in addresses:
+            address = unidecode.unidecode(rough_address).lower()
+
+
+class BrasiliaParser(SinanParser):
+    """ Address parser for Brasilia's DF
+
+    """
+
+    def __init__(self):
+
+        super().__init__()
+        self._re_parser.update(super_quadra_norte=r"(\bsqn\.*\s*)(\d{1,3})",
+                               super_quadra_sul=r"(\bsqs\.*\s*)(\d{1,3})",
+                               quadra=r"(\bq(?:c(?![uadr])|n(?![uadr]))?u?a?d?r?a?\.*\s*[\W_]*)(\d{1,3})",
+                               conjunto=r"(\bco?n?j?u?n?t?o?\.*\s*[\W_]*)(\w{1,3})",
+
+                               rua=r"(\bru?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=condominio|\d)))",
+                               avenida=r"(\bave?n?i?d?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=condominio|\d)))",
+                               condominio=r"(\bcondo?m?i?n?i?o?\.*\s*)((?:\d{1,3})|(?:.+?(?=\d)))",
+                               chacara=r"(\bcha?c?a?r?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=\d)))")
+
 
 
