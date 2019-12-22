846c90a365013e6d304213ce3609c528d2ff6e63
benjamin <benjamin.pillot@ird.fr>
2019-12-22 01:27:30 +0100

    Update

--- apureza/geocoding.py
+++ apureza/geocoding.py
@@ -26,9 +26,17 @@ class SinanParser(XParser):
 
     def __init__(self):
         super().__init__()
-        self._re_parser.update(casa=r"(\b(?:k(?=sa)|c(?=a|s))a?s?a?\.*\s*[\W_]*)(\d{1,3})?",
-                               lote=r"(\blo?t?e?\.*\s*[\W_]*)(\d{1,3})",
-                               apartamento=r"(\bapa?r?t?a?m?e?n?t?o?\.*\s*)(\d{1,3})")
+        self._re_parser.update(casa=r"(\b(?:k(?=sa)|c(?=asa|a?s?a?\.*\s*[\W_]*\d{1,3}))a?s?a?\.*\s*[\W_]*)(\d{1,3})?",
+                               lote=r"(\bl(?=ote|o?t?e?\.*\s*[\W_]*\d{1,3})o?t?e?\.*\s*[\W_]*)(\d{1,3})?",
+                               apartamento=r"(\bap(?=artamento|a?r?t?a?m?e?n?t?o?\.*\s*[\W_]*\d{1,"
+                                           r"3})a?r?t?a?m?e?n?t?o?\.*\s*)(\d{1,3})?",
+
+                               rua=r"(\bru?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=condominio|\d)))",
+                               avenida=r"(\bave?n?i?d?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=condominio|\d)))",
+
+                               condominio=r"(\bcondo?m?i?n?i?o?\.*\s*)((?:\d{1,3})|(?:.+?(?=\d)))",
+                               chacara=r"(\bch(?=acara|a?c?a?r?a?\.*\s*[\W_]*)a?c?a?r?a?\.*\s*)((?:\d{1,3})|(?:.+?("
+                                       r"?=\d)))")
 
     def parse(self, database):
         """ Parse addresses from SINAN database
@@ -48,16 +56,13 @@ class BrasiliaParser(SinanParser):
 
     def __init__(self):
 
-        super().__init__()
         self._re_parser.update(super_quadra_norte=r"(\bsqn\.*\s*)(\d{1,3})",
                                super_quadra_sul=r"(\bsqs\.*\s*)(\d{1,3})",
                                quadra=r"(\bq(?:c(?![uadr])|n(?![uadr]))?u?a?d?r?a?\.*\s*[\W_]*)(\d{1,3})",
-                               conjunto=r"(\bco?n?j?u?n?t?o?\.*\s*[\W_]*)(\w{1,3})",
+                               conjunto=r"(\bco?n?j?u?n?t?o?\.*\s*[\W_]*)(\w{1,3})")
 
-                               rua=r"(\bru?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=condominio|\d)))",
-                               avenida=r"(\bave?n?i?d?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=condominio|\d)))",
-                               condominio=r"(\bcondo?m?i?n?i?o?\.*\s*)((?:\d{1,3})|(?:.+?(?=\d)))",
-                               chacara=r"(\bcha?c?a?r?a?\.*\s*)((?:\d{1,3})|(?:.+?(?=\d)))")
+        # Build superclass after as we wish quadra and conjunto parsed first
+        super().__init__()
 
 
 
