3c175969f0825149936ad89036199dd89818c73f
pillot <benjamin.pillot@ird.fr>
2020-02-11 18:41:06 +0100

    New urban module

--- apureza/urban.py
+++ apureza/urban.py
@@ -12,9 +12,9 @@ __email__ = 'benjaminpillot@riseup.net'
 from fototex.foto import Foto
 from utils.sys.timer import Timer
 
-test = Foto("/home/benjamin/Desktop/APUREZA/FOTO/SUBSET_PLEIADES_20160915_Pan.tif", method="moving")
+test = Foto("/home/benjamin/Desktop/APUREZA/FOTO/SUBSET_PLEIADES_20160915_Pan.tif", method="block")
 for w_size in [7, 11, 15, 19, 21, 25, 29, 35]:
     with Timer() as t:
         test.run(w_size, progress_bar=True, sklearn_pca=True)
     print("spent time (%d): %s" % (w_size, t))
-    test.save_rgb(f"/home/benjamin/Desktop/APUREZA/FOTO/rgb_moving_{w_size}.tif")
+    # test.save_rgb(f"/home/benjamin/Desktop/APUREZA/FOTO/rgb_moving_{w_size}.tif")
